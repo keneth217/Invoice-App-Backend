6f16f85a00074c8b681842da0c07478852e0715b
KENETH217 <kipyegonkeneth03@gmail.com>
2025-07-12 21:08:01 +0300

    Add Swagger api and corrected the database naming to business code

--- src/main/java/com/app/invoice/tenant/repos/business_settings/BusinessSettingsRepository.java
+++ src/main/java/com/app/invoice/tenant/repos/business_settings/BusinessSettingsRepository.java
@@ -1,6 +1,6 @@
 package com.app.invoice.tenant.repos.business_settings;
 
-import com.app.invoice.tenant.entity.school_settings.BusinessSettings;
+import com.app.invoice.tenant.entity.business_settings.BusinessSettings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
